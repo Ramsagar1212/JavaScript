26d24cb0d13367283c255e7f5b5cccb4a51a5c63
Ram sagar <ramsagarchoudhary6@gmail.com>
2024-06-06 19:49:47 +0530

    05_dataType_summary.js

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,32 @@
+let score = 823472
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber )
+//console.log(valueInNumber)
+
+/* 
+ "123" => 123
+ "321abc" => NaN
+  true => 1 , false => 0
+
+*/
+
+
+let isLoggedIn = "ram" 
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn)
+
+// 1 => true , 0 => false 
+// "" => false , "ram" => true
+
+let someNumber = 23
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
