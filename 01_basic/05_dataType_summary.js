b5fd8164b6e353539c9a295c6c1f65da1b57d76e
Ram sagar <ramsagarchoudhary6@gmail.com>
2024-06-13 22:28:05 +0530

    nums

--- 01_basic/05_dataType_summary.js
+++ 01_basic/05_dataType_summary.js
@@ -36,3 +36,29 @@ const myFunction = function(){
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+// there are two type of memory
+/*  1. stack(Primitive)  
+    2. Heap (Non-Primitive)
+*/
+
+let myYoutube  = "ramsagar"
+let anotherName = myYoutube
+anotherName = "chaudhary"
+
+
+console.log(myYoutube);
+console.log(anotherName);
+
+let userOne = {
+    email : "user.com", 
+    upi :  "upi@paytm"
+}
+
+let userTwo = userOne
+
+userTwo.email = "abac@gamil.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
